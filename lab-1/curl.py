4dadb80e1d426a8bafaeea22c0ce6dbf15a765aa
Jihoon Og <og@ualberta.ca>
2021-09-06 11:58:03 -0600

    Added documentation to the script

--- lab-1/curl.py
+++ lab-1/curl.py
@@ -1,5 +1,8 @@
 import requests
 
+# This will send a GET request to the raw URL that points to this file on my personal GitHub. 
+# The default behaviour for a request is that the body is downloaded immediately.
 req = requests.get("https://raw.githubusercontent.com/jihoonog/CMPUT-404-Labs/main/lab-1/curl.py")
 
+# This will print out the source code as plain text
 print(req.text)
