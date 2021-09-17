b01d1be092b740894e023e976d324ec29debf6e2
Jihoon Og <og@ualberta.ca>
2021-09-17 10:55:06 -0600

    Update

--- lab-2/proxy_client.py
+++ lab-2/proxy_client.py
@@ -1,75 +1,34 @@
 #!/usr/bin/env python3
 import socket, sys
+from multiprocessing import Pool
 
 HOST = "localhost"
 PORT = 8001
+BUFFER_SIZE = 1024
 
-#create a tcp socket
-def create_tcp_socket():
-    print('Creating socket')
+payload = "GET / HTTP/1.0\r\nHOST: www.google.com\r\n\r\n"
+
+def connect(addr):
     try:
+        # Create socket, connect, send and recieve, the shutdown
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except (socket.error, msg):
-        print(f'Failed to create socket. Error code: {str(msg[0])} , Error message : {msg[1]}')
-        sys.exit()
-    print('Socket created successfully')
-    return s
+        s.connect(addr)
+        s.sendall(payload.encode())
+        s.shutdown(socket.SHUT_WR)
 
-#get host information
-def get_remote_ip(host):
-    print(f'Getting IP for {host}')
-    try:
-        remote_ip = socket.gethostbyname( host )
-    except socket.gaierror:
-        print ('Hostname could not be resolved. Exiting')
-        sys.exit()
-
-    print (f'Ip address of {host} is {remote_ip}')
-    return remote_ip
-
-#send data to server
-def send_data(serversocket, payload):
-    print("Sending payload")    
-    try:
-        serversocket.sendall(payload.encode())
-    except socket.error:
-        print ('Send failed')
-        sys.exit()
-    print("Payload sent successfully")
-
-def main():
-    try:
-        #define address info, payload, and buffer size
-        host = 'www.google.com'
-        port = 80
-        payload = f'GET / HTTP/1.0\r\nHost: {host}\r\n\r\n'
-        buffer_size = 4096
-
-        #make the socket, get the ip, and connect
-        s = create_tcp_socket()
-
-        remote_ip = get_remote_ip(host)
-
-        s.connect((HOST , PORT))
-        print (f'Socket Connected to {host} on ip {remote_ip}')
-        
-        #send the data and shutdown
-        send_data(s, payload)
-        s.shutdown(socket.SHUT_WR) # shutdown() is different from close(), both close() and shutdown() sends over an EOF signal but close() deallocate the socket resources 
-
-        #continue accepting data until no more left
-        full_data = b""
-        while True:
-            data = s.recv(buffer_size)
-            if not data:
-                 break
-            full_data += data
+        full_data = s.recv(BUFFER_SIZE)
         print(full_data)
+    
     except Exception as e:
         print(e)
     finally:
-        #always close at the end!
+        # Close the socket
         s.close()
+def main():
+    address = [(HOST, PORT)]
+    with Pool() as p:
+        p.map(connect, address * 2)
+
 if __name__ == "__main__":
     main()
 
